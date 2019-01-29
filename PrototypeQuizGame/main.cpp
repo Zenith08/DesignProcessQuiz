e12faba7c5e5dbe2c6601de2929b797190957d2a
Period 2 <period2@earl-r308-m009.local>
2019-01-29 09:16:03 -0800

    Initial Commit

---
+++ PrototypeQuizGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PrototypeQuizGame
+//
+//  Created by Period 2 on 2019-01-29.
+//  Copyright © 2019 Period 2. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
